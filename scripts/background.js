811b038553fcbba99eb76f7b81b4ecc03b0a73a1
chengrady <chengradytmac@163.com>
2018-06-22 16:10:09 +0800

    重构UI

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,15 @@
-﻿var timeJson;
-var timeJsonToday;
-var isUpdateSiteName = false;
-var timeDateStorage;        //总共
-var timeDateStorageToday;       //今天
+﻿let timeJson;
+let timeJsonToday;
+let timeDateStorage;        //总共
+let timeDateStorageToday;       //今天
 
 //从localStorage中读取数据
 getDataFromStorage();
+
 function getDataFromStorage() {
     //获取总数据
     timeDateStorage = localStorage.getItem("timesummary");
-    if (timeDateStorage == null || timeDateStorage == "") {
+    if (timeDateStorage == null || timeDateStorage === "") {
         timeJson = [];
         localStorage.setItem("timesummary", JSON.stringify(timeJson));
     } else {
@@ -17,15 +17,15 @@ function getDataFromStorage() {
     }
     //获取今天数据
     timeDateStorageToday = localStorage.getItem("timesummary_today");
-    var todayStr = getDateStr();
-    if (timeDateStorageToday == null || timeDateStorageToday == "") {
-        timeJsonToday = { "date": "" + todayStr + "", "site": [] };
+    let todayStr = getDateStr();
+    if (timeDateStorageToday == null || timeDateStorageToday === "") {
+        timeJsonToday = {"date": "" + todayStr + "", "site": []};
         localStorage.setItem("timesummary_today", JSON.stringify(timeJsonToday));
         localStorage.setItem("tstotaltime_today", 0);
     } else {
         timeJsonToday = JSON.parse(timeDateStorageToday);
-        if (timeJsonToday.date != todayStr) {
-            timeJsonToday = { "date": "" + todayStr + "", "site": [] };
+        if (timeJsonToday.date !== todayStr) {
+            timeJsonToday = {"date": "" + todayStr + "", "site": []};
             localStorage.setItem("timesummary_today", JSON.stringify(timeJsonToday));
             localStorage.setItem("tstotaltime_today", 0);
         }
@@ -34,12 +34,12 @@ function getDataFromStorage() {
 
 window.setInterval(function () {    //获取当前路径
     chrome.tabs.getSelected(null, function (tab) {
-        var sitedomain = urlToDomain(tab.url);
+        let sitedomain = urlToDomain(tab.url);
         sitedomain = getSingleUrl(sitedomain);
-        if (sitedomain != "") {
-            var todayStr = getDateStr();
-            if (timeJsonToday.date != todayStr) {
-                timeJsonToday = { "date": "" + todayStr + "", "site": [] };
+        if (sitedomain !== "") {
+            let todayStr = getDateStr();
+            if (timeJsonToday.date !== todayStr) {
+                timeJsonToday = {"date": "" + todayStr + "", "site": []};
                 localStorage.setItem("timesummary_today", JSON.stringify(timeJsonToday));
                 localStorage.setItem("tstotaltime_today", 0);
             }
@@ -47,36 +47,36 @@ window.setInterval(function () {    //获取当前路径
             addTimeToSite(sitedomain);
         }
     });
-    if (localStorage.getItem("tstopmsg") != null && localStorage.getItem("tstopmsg") != "" && getTimeStr() == localStorage.getItem("tstopmsg") + ":00") {
+    if (localStorage.getItem("tstopmsg") != null && localStorage.getItem("tstopmsg") !== "" && getTimeStr() === localStorage.getItem("tstopmsg") + ":00") {
         notifyMe();
     }
 }, 1000);
 
 function addTimeToSite(sitedomain) {
-    var isAdded = false;
-    var isAddedToday = false;
+    let isAdded = false;
+    let isAddedToday = false;
     //总数据
-    for (var i = 0; i < timeJson.length; i++) {
-        if (timeJson[i].sitedomain == sitedomain) {
+    for (let i = 0; i < timeJson.length; i++) {
+        if (timeJson[i].sitedomain === sitedomain) {
             timeJson[i].timevalue += 1;
             isAdded = true;
             break;
         }
     }
     if (!isAdded) {
-        var arr = { "sitedomain": sitedomain, "timevalue": 1 }
+        let arr = {"sitedomain": sitedomain, "timevalue": 1}
         timeJson.push(arr);
     }
     //今天数据
-    for (var i = 0; i < timeJsonToday.site.length; i++) {
-        if (timeJsonToday.site[i].sitedomain == sitedomain) {
+    for (let i = 0; i < timeJsonToday.site.length; i++) {
+        if (timeJsonToday.site[i].sitedomain === sitedomain) {
             timeJsonToday.site[i].timevalue += 1;
             isAddedToday = true;
             break;
         }
     }
     if (!isAddedToday) {
-        var arr = { "sitedomain": sitedomain, "timevalue": 1 }
+        let arr = {"sitedomain": sitedomain, "timevalue": 1}
         timeJsonToday.site.push(arr);
     }
 
@@ -86,12 +86,13 @@ function addTimeToSite(sitedomain) {
     isAdded = false;
     isAddedToday = false;
 }
+
 //记录总时间
 function addTimeToTotal() {
-    var totalTime = parseInt(localStorage.getItem("tstotaltime"));
-    if (totalTime == 0 || totalTime == null || isNaN(totalTime)) {
+    let totalTime = parseInt(localStorage.getItem("tstotaltime"));
+    if (totalTime === 0 || totalTime == null || isNaN(totalTime)) {
         totalTime = 1;
-        for (var i = 0; i < timeJson.length; i++) {
+        for (let i = 0; i < timeJson.length; i++) {
             totalTime += timeJson[i].timevalue;
         }
     } else {
@@ -99,34 +100,37 @@ function addTimeToTotal() {
     }
     localStorage.setItem("tstotaltime", totalTime);
 
-    var totalTimeToday = parseInt(localStorage.getItem("tstotaltime_today"));
-    if (totalTimeToday == 0 || totalTimeToday == null || isNaN(totalTimeToday)) {
+    let totalTimeToday = parseInt(localStorage.getItem("tstotaltime_today"));
+    if (totalTimeToday === 0 || totalTimeToday == null || isNaN(totalTimeToday)) {
         totalTimeToday = 1;
     } else {
         totalTimeToday++;
     }
     localStorage.setItem("tstotaltime_today", totalTimeToday);
 }
+
 //把网址变成domain
 function urlToDomain(str) {
-    var siteDomain = "";
+    let siteDomain = "";
     if (isSiteUrl(str)) {
-        var pattern = /^(?:(\w+):\/\/)?(?:(\w+):?(\w+)?@)?([^:\/\?#]+)(?::(\d+))?(\/[^\?#]+)?(?:\?([^#]+))?(?:#(\w+))?/;
+        let pattern = /^(?:(\w+):\/\/)?(?:(\w+):?(\w+)?@)?([^:\/\?#]+)(?::(\d+))?(\/[^\?#]+)?(?:\?([^#]+))?(?:#(\w+))?/;
         siteDomain = pattern.exec(str)[4];
         siteDomain = siteDomain.replace("www.", "");
     }
     return siteDomain;
 }
+
 //判断字符串是否为网址
 function isSiteUrl(url) {
-    var urlpattern = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
-    var objExp = new RegExp(urlpattern);
+    let urlpattern = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+    let objExp = new RegExp(urlpattern);
     return objExp.test(url);
 }
+
 //监听消息
 chrome.extension.onRequest.addListener(
     function (request, sender, sendResponse) {
-        if (request.greeting == "clearhistory") { //清空历史消息
+        if (request.greeting === "clearhistory") { //清空历史消息
             timeJson = [];
             timeJsonToday = {};
             totalTime = 0;
@@ -134,9 +138,10 @@ chrome.extension.onRequest.addListener(
             getDataFromStorage();
         }
     });
+
 function getSingleUrl(stieurl) {
-    var singleUrl = stieurl;
-    for (var i = 0; i < repeatSiteUrl.length; i++) {
+    let singleUrl = stieurl;
+    for (let i = 0; i < repeatSiteUrl.length; i++) {
         if (stieurl.indexOf(repeatSiteUrl[i]) >= 0) {
             singleUrl = repeatSiteUrl[i];
             break;
@@ -144,6 +149,7 @@ function getSingleUrl(stieurl) {
     }
     return singleUrl;
 }
+
 //排行提醒
 function notifyMe() {
     if (!Notification) {
@@ -153,16 +159,16 @@ function notifyMe() {
     if (Notification.permission !== "granted") {
         Notification.requestPermission();
     } else {
-        var timeJsonTodayTemp = jsonSort(timeJsonToday.site, 'timevalue', true);
-        var top1 = getSiteName(timeJsonTodayTemp[0].sitedomain) + "-" + secondToCommonTime(timeJsonTodayTemp[0].timevalue);
-        var top2 = getSiteName(timeJsonTodayTemp[1].sitedomain) + "-" + secondToCommonTime(timeJsonTodayTemp[1].timevalue);
-        var top3 = getSiteName(timeJsonTodayTemp[2].sitedomain) + "-" + secondToCommonTime(timeJsonTodayTemp[2].timevalue);
-        var topnotify = chrome.notifications.create(null, {
+        let timeJsonTodayTemp = jsonSort(timeJsonToday.site, 'timevalue', true);
+        let top1 = getSiteName(timeJsonTodayTemp[0].sitedomain) + "-" + secondToCommonTime(timeJsonTodayTemp[0].timevalue);
+        let top2 = getSiteName(timeJsonTodayTemp[1].sitedomain) + "-" + secondToCommonTime(timeJsonTodayTemp[1].timevalue);
+        let top3 = getSiteName(timeJsonTodayTemp[2].sitedomain) + "-" + secondToCommonTime(timeJsonTodayTemp[2].timevalue);
+        let topnotify = chrome.notifications.create(null, {
             type: 'list',
             iconUrl: 'images/icon_128.png',
             title: '时间脚印-今日浏览排行',
             message: "",
-            items: [{ title: "Top1：", message: top1 }, { title: "Top2：", message: top2 }, { title: "Top3：", message: top3 }]
+            items: [{title: "Top1：", message: top1}, {title: "Top2：", message: top2}, {title: "Top3：", message: top3}]
         });
     }
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,146 +1,18 @@
-﻿var timeJson;
-var timeOrder = true;
-var totalTime = 0;
-var jsonCustomerSiteName;       //用户自定义网站名
-var timeType = 1;       //时间类型：0总、1今天
-
 $(function () {
-    //获取自定义网站json
-    var csnTemp = localStorage.getItem("timesummaryCSN");
-    jsonCustomerSiteName = csnTemp == null || csnTemp == "" ? {} : JSON.parse(csnTemp);
-    setHtml();
-    //时间排序
-    $("#timeorder,#ratioorder").click(function () {
-        timeOrder = !timeOrder;
-        setHtml();
-    });
-    $(".switchbtn").on({
+    $("#switchDetailType").on({
         "click": function () {
-            if (timeType == 1) {
-                timeType = 0;
-                $($(this).children().get(0)).animate({ left: '-28px' }, 'normal');
-                $($(this).children().get(1)).animate({ left: '0' }, 'normal');
-            } else {
-                timeType = 1;
-                $($(this).children().get(1)).animate({ left: '28px' }, 'normal');
-                $($(this).children().get(0)).animate({ left: '0' }, 'normal');
-            }
-            setHtml();
-        },
-        "mouseover": function () {
-            $(".switchbtnfont").css("background-color", "#c9d8e3");
-        },
-        "mouseout": function () {
-            $(".switchbtnfont").css("background-color", "#B0C4DE");
+            popup.switchDetailType();
         }
     });
 });
-//从localStorage中读取数据
-function getDataFromStorage() {
-    var timeDateStorage;
-    if (timeType == 0) {        //总数据
-        timeDateStorage = localStorage.getItem("timesummary");
-    }
-    else {      //今天的数据
-        timeDateStorage = localStorage.getItem("timesummary_today");
-    }
 
-    if (timeDateStorage == null || timeDateStorage == "") {
-        if (timeType == 0) {
-            timeJson = [];
-            localStorage.setItem("timesummary", JSON.stringify(timeJson));
-        } else {
-            timeJson = { "date": "", "site": [] };
-            localStorage.setItem("timesummary_today", JSON.stringify(timeJson));
-        }
-    } else {
-        if (timeType == 0) {
-            timeJson = JSON.parse(timeDateStorage);
-        } else {
-            timeJson = JSON.parse(timeDateStorage).site;
-        }
-        timeJson = jsonSort(timeJson, 'timevalue', timeOrder);
-    }
+var popup = {
+    isToday: true, //展示类型 true：今天、false：总共
+    switchDetailType: function () {
+        $("#detail").animate({
+            left: this.isToday ? "-280px" : "0"
+        }, 'normal');
+        this.isToday = !this.isToday;
+    },
+
 }
-//展示界面
-function setHtml() {
-    let datainfoid = timeType == 1 ? "divperdaydatainfo" : "divtotaldatainfo";
-    var showSiteNum = localStorage.getItem("TMshowsitenum");
-    let sitename = "";      //显示的网站名
-    let sitedomain = "";        //显示的域名
-    let timeofsite = "";        //显示的时间
-    let siteCount = 0;     //显示条数
-    let ratio = 0;
-    let ratioHtml = "";
-    let isShowRatio = localStorage.getItem("showratio");
-    let dataInfoHtml = "";
-    if (isShowRatio != "0") {
-        if (timeType == 0) {
-            totalTime = localStorage.getItem("tstotaltime");
-        } else {
-            totalTime = localStorage.getItem("tstotaltime_today");
-        }
-    } else {
-        $(".spanratio").hide();
-        $("body").css("width", "240px");
-    }
-    getDataFromStorage();
-    if (timeJson.length == 0) {
-        $("#divtitle").hide();
-        $("#" + datainfoid).html("您还没有访问过任何网站！").show(); 
-        if (timeType == 1) {
-            $("#divperdaydatainfo").animate({ left: '0' }, 'normal');
-            $("#divtotaldatainfo").animate({ left: '300px' }, 'normal').hide();
-        } else {
-            $("#divperdaydatainfo").animate({ left: '-300px' }, 'normal').hide();
-            $("#divtotaldatainfo").animate({ left: '0' }, 'normal');
-        }
-        return;
-    } else {
-        $("#" + (timeType == 1 ? "divperdaydatainfo" : "divtotaldatainfo")).show();
-    }
-    if (showSiteNum == null || showSiteNum == 0) {
-        showSiteNum = 15;
-        localStorage.setItem("TMshowsitenum", "15");
-    }
-    siteCount = showSiteNum < timeJson.length ? showSiteNum : timeJson.length;
-    for (var i = 0; i < siteCount; i++) {
-        timeofsite = secondToCommonTime(timeJson[i].timevalue);
-        if (isShowRatio != "0") {
-            ratio = Math.round(timeJson[i].timevalue / totalTime * 1000) / 10.0;
-            ratioHtml = "<span class='spanratio'>" + ratio + "%</span>";
-        }
-        sitedomain = timeJson[i].sitedomain;
-        sitename = getSiteName(sitedomain);
-        dataInfoHtml += "<li><span class='spanname'><span class='spansitename' title='" + sitedomain + "'>" + sitename + "</span><span class='imgupdatenamebtn' title='修改备注' ></span></span><span class='editspan'><input class='inputsitename' value='" + sitename + "' /><span class='imgupdatename' title='确定修改' ></span></span><span class='spantime'>" + timeofsite + "</span>" + ratioHtml + "</li>";
-    }
-    $("#" + datainfoid).html(dataInfoHtml);
-    if (timeType == 1) {
-        $("#divperdaydatainfo").animate({ left: '0' }, 'normal');
-        $("#divtotaldatainfo").animate({ left: '300px' }, 'normal');
-    } else {
-        $("#divperdaydatainfo").animate({ left: '-300px' }, 'normal');
-        $("#divtotaldatainfo").animate({ left: '0' }, 'normal');
-    }
-    //点击网站名直接访问网站
-    if (localStorage.getItem("tsnewtabcheck") == 1) {
-        $(".spansitename").bind({
-            "click": function () {
-                openNewTab("http://" + $(this).attr("title"));
-            }
-        });
-    }
-}
-
-//监听事件
-chrome.extension.onRequest.addListener(
-    function (request, sender, sendResponse) {
-        if (request.greeting == "clearhistory") {
-            setHtml();
-        }
-    });
-
-function openNewTab(openUrl) {
-    chrome.tabs.create({ url: openUrl }, function () { });
-}
-
